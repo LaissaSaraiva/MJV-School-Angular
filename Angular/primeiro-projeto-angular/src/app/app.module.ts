b7cdbb163570b1c47c66feec3c31b5a2fae6cfec
Laissa Saraiva <laissa_saraiva@hotmail.com>
2022-06-14 15:49:32 -0300

    Criação dos componentes main/content/footer e estilização do header e footer para realização do exercícios proposto na aula de Componentes

--- Angular/primeiro-projeto-angular/src/app/app.module.ts
+++ Angular/primeiro-projeto-angular/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { MainComponent } from './home/components/main/main.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ContentComponent } from './home/components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
