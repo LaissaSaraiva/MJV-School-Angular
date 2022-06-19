3b9db5a514b37757f9a170a3ee2a2b71b82eb3de
Laissa Saraiva <laissa_saraiva@hotmail.com>
2022-06-19 18:20:20 -0300

    Lista de Exercicios 1/2/3/4 do "módulo componentes"

---
+++ Angular/projeto-angular-gestao-de-colaboradores/src/app/features/features.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListagemComponent } from './collaborators/listagem/listagem.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListagemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeaturesModule { }
