146f575b64331bea4a2b4c5a0d14ecd5d6614e7b
Laissa Saraiva <laissa_saraiva@hotmail.com>
2022-06-23 17:13:50 -0300

    Exercícios 5/6/7 do módulo 'Módulos e Data Binding'

--- Angular/projeto-angular-gestao-de-colaboradores/src/app/app.component.ts
+++ Angular/projeto-angular-gestao-de-colaboradores/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'projeto-angular-gestao-de-colaboradores';
+  titulo = 'Listagem dos Colaboradores da Empresa @TecnoSoluções';
+
+  colaborador = {
+    id: 7,
+    nome: 'Nathan Carlos',
+    salario: 4500,
+    cargo: 'Dev Pleno',
+  };
 }
---
+++ Angular/projeto-angular-gestao-de-colaboradores/src/app/features/collaborators/collaborators.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListagemComponent } from './components/listagem/listagem.component';
+
+@NgModule({
+  declarations: [
+    ListagemComponent
+  ],
+  imports: [CommonModule],
+  exports: [ListagemComponent]
+})
+export class CollaboratorsModule {}
